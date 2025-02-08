af7b399e4ebdd8a1296aa42e192be6004198efa1
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-02-08 22:08:08 +0100

    first commit

---
+++ contracts/Multisig.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Multisig {
+    address[] public owners;
+    uint public setApprovals;
+    struct Transactions {
+        address to;
+        bool isExecuted;
+        uint amount;
+        uint noOfApprovals;
+    }
+    Transactions[] public transactions;
+
+    //checks which owner approved a transaction
+    mapping(uint => mapping(address => bool)) approvals;
+    mapping(address => bool) public isOwner;
+
+    uint public numberOfTransactions;
+
+    // declare an event that will be ommitted when the transaction is proposed
+    event CreateTransaction(address indexed to, uint indexed amount, uint indexed nonce);
+    event TransactionApproved(uint256 indexed nonce, address indexed owner);
+
+
+    constructor(address[] memory _owners, uint _setApprovals) {
+        require(_owners.length > 0, "At least one owner");
+        require(_setApprovals > 0, "Approvals must be greater than zero");
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            owners.push(owner);
+            isOwner[owner] = true;
+        }
+        setApprovals = _setApprovals;
+    }
+
+    function submitTransaction(address _to, uint _amount) external {
+        require(_amount > 0, "amount sent must be greater than zero");
+        transactions.push(
+            Transactions({
+                to: _to,
+                isExecuted: false,
+                amount: _amount,
+                noOfApprovals: 0
+            })
+        );
+
+        uint nonce = transactions.length - 1;
+        emit CreateTransaction(_to, _amount, nonce);
+    }
+
+    function approveTransactions(uint _nonce) external {
+        // require(msg.sender = owners, "only owners can approve a transaction");
+        approvals[_nonce][msg.sender] = true;
+        transactions[_nonce].noOfApprovals += 1; 
+
+        emit TransactionApproved(_nonce, msg.sender);
+
+        if(transactions[_nonce].noOfApprovals >= setApprovals){
+            // executeTransaction(_nonce);
+        }
+
+    }
+
+    function executeTransaction(uint _nonce) external{
+        
+    }
+}
